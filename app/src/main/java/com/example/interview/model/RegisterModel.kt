f9b16f3ba43402b273ac4b65d4d8333932caaf32
Donapati <donapatisivareddy8@gmail.com>
2023-10-17 14:56:16 +0530

    Registration completed

--- app/src/main/java/com/example/interview/model/RegisterModel.kt
+++ app/src/main/java/com/example/interview/model/RegisterModel.kt
@@ -3,10 +3,8 @@ package com.example.interview.model
 data class RegisterModel (
     val name : String,
     //var name: NameModel? = null,
-
     val mobile: String,
     val email: String,
     //val address: Address? = null,
-
     val password: String
 )
