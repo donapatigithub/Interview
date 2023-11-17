b171c31c35c05498fb3bf4b8d04a9a074bf7b2da
Donapati <donapatisivareddy8@gmail.com>
2023-11-17 11:40:48 +0530

    API in progress

--- app/src/main/java/com/example/interview/model/RetrofitClient.kt
+++ app/src/main/java/com/example/interview/model/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     val apiService : ApiService by lazy {
         Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build().create(ApiService::class.java)
 
